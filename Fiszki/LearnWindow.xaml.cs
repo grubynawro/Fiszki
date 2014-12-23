738042bd5c37d10735562aaf18ddb7233eb5a791
grubynawro <grubynawro@gmail.com>
2014-12-23 23:37:14 +0100

    w chuj zmian, głownie Learnwindow zrobiony (prawie)

--- Fiszki/LearnWindow.xaml.cs
+++ Fiszki/LearnWindow.xaml.cs
@@ -1,39 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Fiszki
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class LearnWindow
     {
         public LearnWindow()
         {
             InitializeComponent();
         }
-
         private void Click_GoBack(object sender, RoutedEventArgs e)
         {
-            this.Close();
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
             new MainWindow().Show();
-          
+            Close();
+        }
+        private void Window_ContentRendered(object sender, EventArgs e)  //do obsługi progressbara w oddzielnym wątku
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(100);
+            }
+        }
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBar.Value = e.ProgressPercentage;
+        }
+        private void Click_Check(object sender, RoutedEventArgs e)  
+        {
+            
+            if (WordLabel.Content.Equals(TranslateBox.Text)) 
+            {
+                AnswerLabel.Foreground = Brushes.LawnGreen;  
+                AnswerLabel.Content = "Bardzo dobrze!";   //tutaj możnaby dodać odpowiedzi pobierane z bazy danych 
+            }                                              //zaleznie jaki wynik jest w danym słowie
+            else
+            {
+                AnswerLabel.Foreground = Brushes.Red;
+                AnswerLabel.Content = "Zjebałeś kolego...";   //tutaj to samo, albo zrobić po kilka, jesli zgadnie lub nie
+            }
+            //tu trzeba zrobić update dla wyniku na danym słowie
         }
 
+        private void Next_Word(object sender, RoutedEventArgs e) //
+        {
+
+            new LearnWindow().Show();
+            Close();
+        }
     }
 }
